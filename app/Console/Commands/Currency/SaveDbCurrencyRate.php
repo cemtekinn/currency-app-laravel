500827710745a89394dab429c7bbf9e0b04503fe
Cem Tekin <cemtekingss@gmail.com>
2024-02-07 11:10:03 +0300

    Currency rates have been added to the database.

--- app/Console/Commands/Currency/SaveDbCurrencyRate.php
+++ app/Console/Commands/Currency/SaveDbCurrencyRate.php
@@ -2,19 +2,30 @@
 
 namespace App\Console\Commands\Currency;
 
+use App\Models\CurrencyRate;
 use Illuminate\Console\Command;
 use App\Services\CurrencyService;
 
 class SaveDbCurrencyRate extends Command
 {
     
-    protected $signature = 'currency:save';
+    protected $signature = 'currency:update';
     protected $description = 'Bu komut ile döviz kurlarını veritabanına kaydetebilirsiniz';
 
     public function handle()
     {
         $currencyService = new CurrencyService();
         $currencyData = $currencyService->getCurrencyData();
-        
+
+        CurrencyRate::where('name', 'usd')->update([
+            'selling_rate' => $currencyData['usdSellingRate'],
+            'buying_rate' => $currencyData['usdBuyingRate']
+        ]);
+    
+        CurrencyRate::where('name', 'euro')->update([
+            'selling_rate' => $currencyData['euroSellingRate'],
+            'buying_rate' => $currencyData['euroBuyingRate']
+        ]);
+        $this->info('Döviz kurları başarıyla güncellendi.');
     }
 }
